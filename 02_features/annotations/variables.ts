99bd6a08de85451dcafb1fb038e805897d14bcd8
mosesvk <kaumatule93@gmail.com>
2022-01-31 19:48:13 -0700

    sec. 3 type annotations

--- 02_features/annotations/variables.ts
+++ 02_features/annotations/variables.ts
@@ -35,11 +35,44 @@ const logNumber: (i: number) => void = (i: number) => {
   console.log(i)
 };
 
-// When to use annotations
+// WHEN TO USE TYPE ANNOTATION
 // 1. Function that returns the 'any' type
-const json = '{"x": 10, "y": 20}'
-const coordinates = JSON.parse(json)
-console.log(coordinates); // {x: 10, y: 20}
+  const json = '{"x": 10, "y": 20}'
+  const coordinates = JSON.parse(json)
+  console.log(coordinates); // {x: 10, y: 20}
+  coordinates.asdfjafads
+  // Typescript will NOT catch this error because we used JSON.parse which only returns a type of ANY. 
+
+  // How do we fix the above problem? 
+  const newCoordinates: {x: number; y: number} = JSON.parse(json)
+  // newCoordinates.asdfjafads
+  // NOW Typescript will catch this error above because we added the type Annotation for the json object 
+
+// 2. Declaring variable in one line then initializing it later
+  let words = ['red', 'green', 'blue']
+  let foundWord: boolean;
+  // let foundWord = false 
+  // Would be the best use case which will allow Type inference.
+  
+  for (let i = 0; i < words.length; i++) {
+    if (words[i] === 'green') {
+      foundWord = true
+    }
+  }
+
+// 3. We want a variable to have a type that can't be inferred
+  let numbers = [-10, -1, 12]
+  let numberAboveZero: boolean | number = false;
+
+  for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] > 0) {
+      numberAboveZero = numbers[i]
+    }
+  }
+
+
+
+
 
 
 
