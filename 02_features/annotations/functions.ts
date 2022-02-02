226c4ffb11c790bb9bd7f391828e373f17dc15e7
mosesvk <kaumatule93@gmail.com>
2022-02-02 09:44:04 -0700

    intro to annotations around functions

--- 02_features/annotations/functions.ts
+++ 02_features/annotations/functions.ts
@@ -10,3 +10,13 @@ const subtract = (a: number, b: number): number => {
 const logger = (message: string): void => {
   console.log(message)
 }
+
+
+const throwError = (message: string): string => {
+  
+  if (!message){
+    throw new Error(message) 
+  }
+
+  return message
+}
