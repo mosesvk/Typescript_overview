29facde9e1b8722b128dae92852b518b17b30c70
mosesvk <kaumatule93@gmail.com>
2022-02-02 09:41:52 -0700

    intro to annotations around functions

--- 02_features/annotations/functions.ts
+++ 02_features/annotations/functions.ts
@@ -1,4 +1,12 @@
 const add = (a: number, b: number): number => {
-
   return a + b
+}
+
+const subtract = (a: number, b: number): number => {
+  return a - b
+}
+
+// we use void to say there will NOT be a return value in the function
+const logger = (message: string): void => {
+  console.log(message)
 }
