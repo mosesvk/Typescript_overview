4c8f0839a983e46566bbad6dfc58a9221c8b0533
mosesvk <kaumatule93@gmail.com>
2022-02-08 10:38:54 -0700

    Object and nested object Types

--- _Main/app.ts
+++ _Main/app.ts
@@ -1,19 +1,6 @@
-const add = (n1: number, n2: number, showResult: boolean, phrase: string) => {
+const person = {
+  name: 'Mo Money',
+  age: 28,
+};
 
-  const result = n1 + n2;
-
-  if (showResult){
-    console.log(phrase + result)
-  } else {
-    return n1 + n2
-  }
-
-}
-
-// const number1 = '5'
-const number1 = 5
-const number2 = 2.8
-const printResult = true
-const resultPhrase = 'Result is: '
-
-add(number1, number2, printResult, resultPhrase)
+console.log(person.name);
