bf77091726a0a2b915b833703e2615e858fbe989
mosesvk <kaumatule93@gmail.com>
2022-02-07 22:08:33 -0700

    starter project

--- 01_starter_project/js_only_pre.js
+++ 01_starter_project/js_only_pre.js
@@ -4,8 +4,10 @@ const input2 = document.getElementById("num2");
 
 function add(num1, num2) {
   return num1 + num2;
+  // These will be concatenated because these values are strings
 }
 
 button.addEventListener("click", function() {
   console.log(add(input1.value, input2.value));
-});
+  // When you access an input element value, it is ALWAYS a String
+}); 
