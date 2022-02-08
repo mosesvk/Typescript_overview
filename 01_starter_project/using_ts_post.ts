5adcc78b5aed80ad17da5905b9a796d3d0de9941
mosesvk <kaumatule93@gmail.com>
2022-02-07 22:26:09 -0700

    starter project

--- 01_starter_project/using_ts_post.ts
+++ 01_starter_project/using_ts_post.ts
@@ -1,12 +1,13 @@
-const button1 = document.querySelector("button");
-const input_1 = document.getElementById("num1")! as HTMLInputElement;
-const input_2 = document.getElementById("num2")! as HTMLInputElement;
-// This is called TYPE CASTING which tells Typescript that this is For Sure am Input Element with a value in which there WILL be a value. And now the error on line 11 is gone 
+// const button1 = document.querySelector("button");
+// const input_1 = document.getElementById("num1")! as HTMLInputElement;
+// const input_2 = document.getElementById("num2")! as HTMLInputElement;
+// // This is called TYPE CASTING which tells Typescript that this is For Sure am Input Element with a value in which there WILL be a value. And now the error on line 11 is gone 
 
-function add(num1, num2) {
-    return num1 + num2;
-}
+// function add(num1: number, num2: number) {
+//     return num1 + num2;
+// }
 
-button.addEventListener("click", function() {
-  console.log(add(input_1.value, input_2.value));
-}); 
+// button1.addEventListener("click", function() {
+//   console.log(add(+input_1.value, +input_2.value));
+//   // We have to add the '+' to the values because they are initially strings (Remember input values are automatically strings)
+// }); 
--- 01_starter_project/using_ts_pre.ts
+++ 01_starter_project/using_ts_pre.ts
@@ -1,13 +1,13 @@
-const button = document.querySelector("button");
-const input1 = document.getElementById("num1");
-const input2 = document.getElementById("num2");
-// Typescript already recognizes that these variables above have already been declared (along with the Function in line 6) in other files within the same folder
+// const button = document.querySelector("button");
+// const input1 = document.getElementById("num1");
+// const input2 = document.getElementById("num2");
+// // Typescript already recognizes that these variables above have already been declared (along with the Function in line 6) in other files within the same folder
 
-function add(num1, num2) {
-    return num1 + num2;
-}
+// function add(num1, num2) {
+//     return num1 + num2;
+// }
 
-button.addEventListener("click", function() {
-  console.log(add(input1.value, input2.value));
-  // Even if we change the above variable names (Go ahead and try it) We would still get errors above because Typescript is NOT Sure that the 'input1 has a value'
-}); 
+// button.addEventListener("click", function() {
+//   console.log(add(input1.value, input2.value));
+//   // Even if we change the above variable names (Go ahead and try it) We would still get errors above because Typescript is NOT Sure that the 'input1 has a value'
+// }); 
