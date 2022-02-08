84b1102e184cedd6d4a4c1ed69f52ec0694f40b2
mosesvk <kaumatule93@gmail.com>
2022-02-08 12:22:48 -0700

    Array Types

--- _Main/app.ts
+++ _Main/app.ts
@@ -1,6 +1,15 @@
 const person = {
   name: 'Mo Money',
   age: 28,
+  hobbies: ['Sports', 'Piano']
 };
 
+let favoriteActivities: string[];
+favoriteActivities = ['Sports']
+
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase())
+}
