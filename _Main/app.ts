82557756832ea2bf3ab6acf3fc4d96163309553c
mosesvk <kaumatule93@gmail.com>
2022-02-09 12:24:36 -0700

    function types

--- _Main/app.ts
+++ _Main/app.ts
@@ -1,21 +1,22 @@
-const addAndHandle = (n1: number, n2: number, cb: (num: number) => void) => {
-  const result = n1 + n2;
-  cb(result);
-  // if we were to add any other parameter to cb then we would get an error because we only specified one parameter in the cb function parameter.
-};
+let userInput: unknown;
+let userName: string
 
-addAndHandle(10, 20, (result) => {
-  console.log(result);
-});
-// we don't have to specify types here because we already did above in the addAndHandle function when using the cb(callback), we already specified that it would take in a number.
-
-
-function sendRequest(data: string, cb: (response: any) => void) {
-  // ... sending a request with "data"
-  return cb({ data: 'Hi there!' });
+userInput = 5;
+userInput = 'Max'
+if (typeof userInput === 'string') {
+  userName = userInput
 }
 
-sendRequest('Send this!', (response) => {
-  console.log(response);
-  return true;
-});
+// unknown is still a type you will rarely use but it is STILL a better type than <any>
+
+
+const generateError =(message: string, code: number): never => {
+  throw {
+    message: message, 
+    errorCode: code
+  }
+}
+
+generateError('An error occured!', 500)
+
+// this function is not only void but a NEVER. Because it will crash and break the script because we are intentionally adding the NEVER
