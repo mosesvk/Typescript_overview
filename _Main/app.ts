e12d03b6307957e6066d857dd5ae134bba71b4b1
mosesvk <kaumatule93@gmail.com>
2022-02-10 08:46:42 -0700

    update

--- _Main/app.ts
+++ _Main/app.ts
@@ -1,6 +1,5 @@
-let age: number;
-age = 30;
-const username = "moses"
+const button = document.querySelector('button')
 
-
-console.log(username)
+button?.addEventListener('click', () => {
+  console.log('Clicked')
+})
