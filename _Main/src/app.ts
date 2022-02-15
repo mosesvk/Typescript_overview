b3d8909ab644d803f66426f2cc6ee67a256ae184
mosesvk <kaumatule93@gmail.com>
2022-02-15 14:45:53 -0700

    compile new Class Department

--- _Main/src/app.ts
+++ _Main/src/app.ts
@@ -1,3 +1,13 @@
 class Department {
-  
+  name: string;
+
+  constructor(n: string) {
+    this.name = n;
+  }
 }
+
+// We have to pass the contstructor parameter to this.
+const accounting = new Department('Accounting');
+
+console.log(accounting); 
+// -> Department {name: 'Accounting'}
