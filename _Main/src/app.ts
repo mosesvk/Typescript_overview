c8ca39c81b0eb5f8ae03e918914f2be20e38fd89
mosesvk <kaumatule93@gmail.com>
2022-02-15 17:22:27 -0700

    constructor functions & THIS keyword

--- _Main/src/app.ts
+++ _Main/src/app.ts
@@ -4,6 +4,13 @@ class Department {
   constructor(n: string) {
     this.name = n;
   }
+
+  describe(this: Department) {
+    // "this: Department" will make this method more strict saying that describe() has to be added onto an Instance of Department OR it HAS to have a Name Property like the Department Class
+
+    console.log('Department: ' + this.name)
+    // -> will point to ANY object with a NAME property. remember, it doesn't only have to be a Copy of the Department class.
+  }
 }
 
 // We have to pass the contstructor parameter to this.
@@ -11,3 +18,7 @@ const accounting = new Department('Accounting');
 
 console.log(accounting); 
 // -> Department {name: 'Accounting'}
+
+accounting.describe()
+// points to describe() in the class
+// -> Department: Accounting
