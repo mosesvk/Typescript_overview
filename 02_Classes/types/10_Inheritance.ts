44a30778fb7368d1ad07cc615415155f8f809d12
mosesvk <kaumatule93@gmail.com>
2022-02-17 21:34:02 -0700

    Inheritance

---
+++ 02_Classes/types/10_Inheritance.ts
@@ -0,0 +1,64 @@
+class Department {
+  // private id: string;
+  // private name: string;
+  // private employees: string[] = [];
+  // private makes 'employees' only accessible from INSIDE the Class. So any method inside the class is still able to work with employees.
+
+  constructor(
+    private readonly id: string,
+    private name: string,
+    private employees: string[] = []
+  ) {
+    // this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log(`Department (${this.id}): ${this.name}`);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
+  }
+}
+
+// We will now create different Departments using INHERITANCE. they will inherit Department object.  
+
+class ITDepartment extends Department { 
+  admins: string[]; 
+  constructor(id: string, admins: string[]) {
+    super(id, 'IT'); 
+    this.admins = admins
+  }
+}
+
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id, 'Accounting')
+  }
+
+  addReport(text: string) {
+    this.reports.push(text)
+  }
+
+  printReports() {
+    console.log(this.reports)
+  }
+}
+
+const it = new ITDepartment('d1', ['Mo, Solia']);
+
+it.addEmployee('max');
+it.addEmployee('trey');
+it.describe();
+it.printEmployeeInformation();
+console.log(it)
+
+const accounting = new AccountingDepartment('d2', [])
+
+accounting.addReport('Something went wrong...')
+accounting.printReports();
