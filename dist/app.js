91de935dbad6244c990da8c309d7eaacabd266fc
mosesvk <kaumatule93@gmail.com>
2022-02-28 19:38:26 -0700

    structure project

--- dist/app.js
+++ dist/app.js
@@ -2,7 +2,7 @@
 class ProjectInput {
     constructor() {
         this.templateElement = document.getElementById('project-input');
-        this.hostElement = document.getElementById('root');
+        this.hostElement = document.getElementById('app');
         const importedNode = document.importNode(this.templateElement.content, true);
         this.element = importedNode.firstElementChild;
         this.attach();
--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ class ProjectInput {
   constructor() {
     // ! is telling Typescript that we are 100% sure that we will get an that ID.
     this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement
-    this.hostElement = document.getElementById('root')! as HTMLDivElement
+    this.hostElement = document.getElementById('app')! as HTMLDivElement
 
     const importedNode = document.importNode(this.templateElement.content, true);
     this.element = importedNode.firstElementChild as HTMLFormElement
