587492db88d2a88e88d41e09157c0205c6ecc1a5
mosesvk <kaumatule93@gmail.com>
2022-03-07 13:47:54 -0700

    todo component

--- src/app.ts
+++ src/app.ts
@@ -9,7 +9,8 @@ class ProjectInput {
     this.hostElement = document.getElementById('app')! as HTMLDivElement
 
     const importedNode = document.importNode(this.templateElement.content, true);
-    this.element = importedNode.firstElementChild as HTMLFormElement
+    this.element = importedNode.firstElementChild as HTMLFormElement;
+    this.element.id = 'user-input';
     this.attach()
   }
 
